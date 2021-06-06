85246a16784da28d4fdc49789a35729ea6613b97
Christian Baumann <christian.baumann@shipsta.com>
2021-06-06 11:10:43 +0200

    Chapter 4 (parameterized tests w/ @CsvSource) done

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -1,16 +1,13 @@
 package junit5tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class ParameterizedTests {
 
     @ParameterizedTest(name = "Run: {index}  -  value: {arguments}")
     @ValueSource(ints = {1, 5, 6})
-    void intValues (int theParam){
+    void intValues(int theParam) {
         System.out.println("theParam = " + theParam);
     }
 
@@ -19,7 +16,31 @@ public class ParameterizedTests {
 //    @EmptySource
     @NullAndEmptySource
     @ValueSource(strings = {"firstString", "secondString"})
-    void stringValues (String theParam) {
+    void stringValues(String theParam) {
         System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,rogers", "captain,marvel", "bucky,barnes"})
+    void csvSource_StringString(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,32,true", "captain,21,false", "bucky,5,true"})
+    void csvSource_StringIntBoolean(String param1, int param2, boolean param3) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america,'steve,rogers'", "winter soldier,'bucky,barnes'"})
+    void csvSource_StringWithComma(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+    void csvSource_StringWithDiffDelimiter(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
     }
 }
