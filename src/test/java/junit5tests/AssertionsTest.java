84f1f1f06cfe73014dcfef8ba6b064d8439bfdb6
Christian Baumann <christian.baumann@shipsta.com>
2021-06-06 13:53:51 +0200

    Chapter 16 (Test Watcher) done

--- src/test/java/junit5tests/AssertionsTest.java
+++ src/test/java/junit5tests/AssertionsTest.java
@@ -1,7 +1,9 @@
 package junit5tests;
 
+import listeners.Listener;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -12,6 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(Listener.class)
 public class AssertionsTest {
 
     @Test
